8ab0912458518e54ed2f628b1fe3ecd5ca6ed9f7
Loai Al-Sharee <loaialsharee1@gmail.com>
2020-04-11 22:55:02 +0800

    Header Added

--- weshare_main/lib/screens/settings.dart
+++ weshare_main/lib/screens/settings.dart
@@ -14,7 +14,26 @@ class Settings extends StatelessWidget {
             ),
             padding: EdgeInsets.symmetric(horizontal: 22),
             child: Row(
-              children: <Widget>[],
+              children: <Widget>[
+                // Settings Header Components
+                SizedBox(
+                  width: 50,
+                  child: FlatButton(
+                    child: Icon(Icons.arrow_back),
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  child: Text(
+                    'Settings',
+                    style: Theme.of(context).appBarTheme.textTheme.title,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
