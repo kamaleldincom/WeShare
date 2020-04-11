6731ada2b2751f7466616a815e50b63cf66b4e8f
Ahmed Kamal Eldin <ahmed@kamaleldin.com>
2020-04-11 23:20:01 +0800

    setupmain

--- weshare_main/lib/main.dart
+++ weshare_main/lib/main.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:weshare_main/screens/Dashboard.dart';
+import 'package:weshare_main/screens/Notifications.dart';
+import 'package:weshare_main/screens/Profile.dart';
+import 'package:weshare_main/screens/Rides.dart';
+import 'package:weshare_main/screens/authentication/Login.dart';
+import 'package:weshare_main/screens/constants.dart';
+import 'package:weshare_main/screens/splash.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui';
+import './screens/settings.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
 
-  // the routes will be changed as we progress in the work
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
 
-  initialRoute: '/',
-  routes: {
-    // '/': (context) => Loading(),
-    // '/home': (context) => Home(),
-    // '/settings': (context) => Settings(),
-
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        // fontFamily: 'SegoeUI',
+        textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+              //   fontFamily: 'SegoeUI',
+              fontSize: 16,
+            )),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  //  fontFamily: 'SegoeUI',
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Settings(),
+      initialRoute: '/splash',
+      routes: {
+        // '/': (context) => Loading(),
+        // '/home': (context) => Home(),
+        '/settings': (context) => Settings(),
+        '/login': (context) => Login(),
+        '/splash': (context) => Splash(),
+        '/profile': (context) => Profile(),
+        '/notifications': (context) => Notifications(),
+        '/rides': (context) => Rides(),
+        '/dashboard': (context) => Dashboard(),
+        '/nav': (context) => BtmNavBar(),
+      },
+    );
   }
-));
-
-
+}
