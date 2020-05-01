e5714d92ca4b17b4bf74a6b7bf03c9ba1e2e87cb
Abdulrahman al-abidi <apody3000@gmail.com>
2020-05-02 03:06:27 +0800

    signUp layout ready

---
+++ weshare_main/lib/screens/authentication/auth_consts.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+Column bottomSection(String text) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        // 'Forgot Password?',
+        text,
+        style: TextStyle(color: Colors.blue, fontSize: 14.0),
+      ),
+      Divider(),
+      Text('Or Sign-up using ',
+          textAlign: TextAlign.start,
+          style: TextStyle(color: Colors.grey, fontSize: 18.0)),
+      SizedBox(height: 40),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Image.asset('assets/icon-google.png'),
+          Image.asset('assets/icon-facebook.png'),
+        ],
+      ),
+    ],
+  );
+}
