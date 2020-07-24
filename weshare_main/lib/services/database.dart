fbd1b8caba81bd4d35ffb136e052439ae7e1df68
Abdulrahman al-abidi <apody3000@gmail.com>
2020-07-24 20:37:45 +0800

    hotfix

--- weshare_main/lib/services/database.dart
+++ weshare_main/lib/services/database.dart
@@ -332,10 +332,3 @@ Image m;
     });
   }
 }
-
-class FireStorageService extends ChangeNotifier {
-  FireStorageService();
-  static Future<dynamic> loadFromStorage(BuildContext context, String image) async {
-    return await FirebaseStorage.instance.ref().child(image).getDownloadURL();
-  }
-}
